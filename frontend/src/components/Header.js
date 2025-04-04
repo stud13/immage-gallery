a82017d1471afc58cef30f2096547b96e052563e
Wertlexx <stud13.exe@gmail.com>
2025-04-04 14:19:48 +0300

    fixed code with linter

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
 
 const navbarStyle = {
-    backgroundColor: 'lightblue'
+  backgroundColor: 'lightblue',
 };
 
 const Header = (props) => {
-    return (
-        <Navbar style={navbarStyle} variant="light">
-            <Container>
-                <Navbar.Brand href="/">{props.title}</Navbar.Brand>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,35 +1,41 @@
-import React from "react";
+import React from 'react';
 //import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
-const Search = ({word, setWord, handleSubmit}) => {
-    return(
-        <Container className="mt-4">
-            <Row className="justify-content-center">
-                <Col xs={12} md={8}>
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group as={Row}>
-                            <Col xs={9}>
-                                <Form.Control
-                                    type="text"
-                                    value={word}
-                                    onChange={(e) => setWord(e.target.value)}
-                                    placeholder="Search for new image" 
-                                />
-                            </Col>
-                            <Col>
-                                <Button onClick={() => setWord("")} variant="primary" type="submit">Search</Button>
-                            </Col>
-                        </Form.Group>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-    );
+const Search = ({ word, setWord, handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group as={Row}>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image"
+                />
+              </Col>
+              <Col>
+                <Button
+                  onClick={() => setWord('')}
+                  variant="primary"
+                  type="submit"
+                >
+                  Search
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Search;
