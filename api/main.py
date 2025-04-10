bce2dcdc795bf2eb3fa60a458f62328bba32b9af
Wertlexx <stud13.exe@gmail.com>
2025-04-10 17:35:04 +0300

    sample flask app

--- api/main.py
+++ api/main.py
@@ -1,1 +1,10 @@
-print("hello from python")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "hello world"
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5050)
